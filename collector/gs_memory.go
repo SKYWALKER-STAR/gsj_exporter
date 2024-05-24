2b2622a393ff9b365ec0b425838ed856319211a8
ming <ming@localhost.localdomain>
2024-05-23 23:34:17 -0400

    增加告警指标: 1. gs_query_slow_query 2. gs_query_response_query_p80 3. gs_query_response_query_p95 4. gs_memory_max_process_memory 5. gs_memory_max_process_memory

---
+++ collector/gs_memory.go
@@ -0,0 +1,101 @@
+// Copyright 2022 The Prometheus Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package collector
+
+import (
+	_ "fmt"
+	"context"
+	"database/sql"
+
+	_ "github.com/go-kit/log"
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+const MemorySubsystem = "memory"
+
+func init() {
+	registerCollector(MemorySubsystem, defaultEnabled, NewPGMemoryCollector)
+}
+
+type PGMemoryCollector struct {
+}
+
+func NewPGMemoryCollector() (Collector, error) {
+	return &PGMemoryCollector{}, nil
+}
+
+var (
+	gsProcessMemoryInfo = prometheus.NewDesc(
+		prometheus.BuildFQName(
+			namespace,
+			MemorySubsystem,
+			"gs_memory_info",
+		),
+		"Max process memory",
+		[]string{"nodename","memtype"}, nil,
+	)
+
+	gsProcessMemoryInfoQuery	= "SELECT * FROM dbe_perf.MEMORY_NODE_DETAIL;"
+)
+
+// Update implements Collector and exposes database size.
+// It is called by the Prometheus registry when collecting metrics.
+// The list of databases is retrieved from pg_database and filtered
+// by the excludeMemory config parameter. The tradeoff here is that
+// we have to query the list of databases and then query the size of
+// each database individually. This is because we can't filter the
+// list of databases in the query because the list of excluded
+// databases is dynamic.
+func (c PGMemoryCollector) Update(ctx context.Context, instance *instance, ch chan<- prometheus.Metric) error {
+	
+	db := instance.getDB()
+
+	// Memory total amount of slow sql
+	//rows, err := db.MemoryContext(ctx,
+	//	pgSlowSQLMemory,
+	//)
+	rows, err := db.QueryContext(ctx,
+		gsProcessMemoryInfoQuery,
+	)
+	if err != nil {
+		return err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var nodename,memtype sql.NullString
+		var membytes sql.NullFloat64
+
+		err := rows.Scan(
+			&nodename,
+			&memtype,
+			&membytes)
+
+		if err != nil {
+			return err
+		}
+
+		labels := []string{nodename.String,memtype.String}
+
+		ch <- prometheus.MustNewConstMetric(
+			gsProcessMemoryInfo,
+			prometheus.GaugeValue, 
+			membytes.Float64,
+			labels...,
+		)
+
+	}
+	return nil
+
+}
---
+++ collector/pg_stat_sql.go
@@ -0,0 +1,107 @@
+// Copyright 2022 The Prometheus Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package collector
+
+import (
+	"fmt"
+	"context"
+	"database/sql"
+
+	_ "github.com/go-kit/log"
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+const QuerySubsystem = "query"
+
+func init() {
+	registerCollector(QuerySubsystem, defaultEnabled, NewPGQueryCollector)
+}
+
+type PGQueryCollector struct {
+}
+
+func NewPGQueryCollector() (Collector, error) {
+	return &PGQueryCollector{}, nil
+}
+
+var (
+	pgSlowSQL = prometheus.NewDesc(
+		prometheus.BuildFQName(
+			namespace,
+			QuerySubsystem,
+			"slow_sql",
+		),
+		"slow sql information",
+		[]string{"datname","pid","sessionid","usename","application_name","client_addr","client_port","query_id","query","query_start"}, nil,
+	)
+
+	pgSlowSQLQuery			= "SELECT datname,pid,sessionid,usename,application_name,client_addr,client_port,query_id,query,query_start FROM pg_stat_activity WHERE state != 'idle' AND now()-query_start > interval '5s' ORDER BY query_start;"
+)
+
+// Update implements Collector and exposes database size.
+// It is called by the Prometheus registry when collecting metrics.
+// The list of databases is retrieved from pg_database and filtered
+// by the excludeQuery config parameter. The tradeoff here is that
+// we have to query the list of databases and then query the size of
+// each database individually. This is because we can't filter the
+// list of databases in the query because the list of excluded
+// databases is dynamic.
+func (c PGQueryCollector) Update(ctx context.Context, instance *instance, ch chan<- prometheus.Metric) error {
+	
+	db := instance.getDB()
+
+	// Query total amount of slow sql
+	//rows, err := db.QueryContext(ctx,
+	//	pgSlowSQLQuery,
+	//)
+	rows, err := db.QueryContext(ctx,
+		pgSlowSQLQuery,
+	)
+	if err != nil {
+		return err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var datname,pid,sessionid,usename,application_name,client_addr,client_port,query_id,query,query_start sql.NullString
+		err := rows.Scan(
+			&datname,
+			&pid,
+			&sessionid,
+			&usename,
+			&application_name,
+			&client_addr,
+			&client_port,
+			&query_id,
+			&query,
+			&query_start)
+
+		if err != nil {
+			return err
+		}
+
+		labels := []string{datname.String,pid.String,sessionid.String,usename.String,application_name.String,client_addr.String,client_port.String,query_id.String,query.String,query_start.String}
+
+		fmt.Println(labels)
+		ch <- prometheus.MustNewConstMetric(
+			pgSlowSQL,
+			prometheus.GaugeValue, 
+			1,
+			labels...,
+		)
+
+	}
+	return nil
+
+}
